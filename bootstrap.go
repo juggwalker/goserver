9158b25362cee8d1e6abac03e37f0325d91e5ee9
jugg.walker <jugg.walker@qq.com>
2018-07-27 01:49:58 +0800

    log

--- bootstrap.go
+++ bootstrap.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+var log = logrus.New()
+
 func main() {
+	log.Out = os.Stdout
 	go FileServ()
 	go HttpServ()
+
+	log.WithFields(logrus.Fields{
+		"file": "success",
+		"http": true,
+	}).Info("server is yet!")
+
 	select {}
 }
